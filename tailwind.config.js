19e97a09fdb380874f8ad023bf50a64c327a3219
KsanaSal <oasalivon@gmail.com>
2023-08-15 16:07:52 +0300

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js}", "./index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
