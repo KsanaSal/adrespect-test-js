a716ff3e8b2d72627d5e8b7dbf8e5c0cc379302e
KsanaSal <oasalivon@gmail.com>
2023-08-17 17:36:46 +0300

    added images, icons and styles

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,18 +1,28 @@
-import javascriptLogo from "../../../javascript.svg";
+import logoSite from "../../images/logo/giarddesign.svg";
+import search from "../../images/icons/search-icon.svg";
+import arrowDown from "../../images/icons/arrow-down-icon.svg";
 
 const header = document.createElement("header");
-const headerNav = `
-    <img src="${javascriptLogo}" class="logo" alt="JavaScript logo" />
-    <nav>
-        <ul>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/about">About</a></li>
+header.classList.add("flex", "w-[1262px]", "justify-between", "py-[24px]");
+const headerNav = /*html*/ `
+    <a href="/">
+        <img src="${logoSite}" class="w-[115px] h-[19px]" alt="Giarddesign" />
+    </a>
+    <nav class="flex flex-row gap-[48px] items-center">
+        <ul class="flex flex-row gap-[48px] items-center">
+            <li class="flex flex-row gap-[5px] items-center">
+                <a href="#oferta">Oferta</a>
+                <img src="${arrowDown}" class="w-[12px] h-[12px]" alt="Search"/>
+            </li>
+            <li><a href="#firma">O firmie</a></li>
+            <li><a href="#realizacje">Realizacje</a></li>
+            <li><a href="#kontakt">Kontakt</a></li>
         </ul>
+        <button>
+            <img src="${search}" class="w-[24px] h-[24px]" alt="Search" />
+        </button>
     </nav>
-    <button></button>
-    `;
+`;
 
 header.innerHTML = headerNav;
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 import "../../style.css";
 import header from "./components/header";
-// import javascriptLogo from "../javascript.svg";
-// import viteLogo from "/vite.svg";
-// import { setupCounter } from "./counter.js";
 
 const app = document.querySelector("#app");
 
