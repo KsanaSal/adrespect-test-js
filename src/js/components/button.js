a9ceb1b391ce1c2a8c8e8989d293d5b9b56a60a7
KsanaSal <oasalivon@gmail.com>
2023-08-19 22:40:35 +0300

    added content about firm

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,14 +1,17 @@
 export const buttonPrimary = (title) => {
     return /*html*/ `
-    <button type="button" class="transition ease-in-out delay-150 bg-primary py-[12px] px-[24px] w-max rounded-[200px] text-[16px] text-grayLight hover:-translate-y-1 duration-300 hover:scale-105 hover:shadow-box hover:text-primary hover:bg-grayLight border-2 border-primary">
+    <button type="button" class="transition ease-in-out delay-150 bg-primary py-[12px] px-[24px] w-max rounded-[200px] text-[16px] text-grayLight hover:-translate-y-1 duration-300 hover:scale-105 hover:shadow-box hover:text-primary hover:bg-inherit border-2 border-primary">
         ${title}
     </button>
     `;
 };
 
-export const buttonSecondary = (title) => {
+export const buttonSecondary = (
+    title,
+    className = "text-primary border-primary hover:text-grayLight hover:bg-primary"
+) => {
     return /*html*/ `
-    <button type="button" class="transition ease-in-out delay-150 bg-grayLight py-[12px] px-[22px] w-max rounded-[200px] text-[16px] text-primary hover:-translate-y-1 duration-300 hover:scale-105 hover:shadow-box hover:text-grayLight hover:bg-primary border-2 border-primary">
+    <button type="button" class="${className} transition ease-in-out delay-150 bg-inherit py-[12px] px-[22px] w-max rounded-[200px] text-[16px]  hover:-translate-y-1 duration-300 hover:scale-105 hover:shadow-box border-2">
         ${title}
     </button>
     `;
