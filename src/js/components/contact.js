9aca146a699edaabc53ca166720417364dce6b7c
KsanaSal <oasalivon@gmail.com>
2023-08-20 21:45:32 +0300

    added contact

---
+++ src/js/components/contact.js
@@ -0,0 +1,36 @@
+import { buttonPrimary } from "./button";
+
+const contact = document.createElement("section");
+
+contact.classList.add(
+    "w-full",
+    "bg-secondary",
+    "py-[30px]",
+    "md:py-[40px]",
+    "lg:py-[60px]",
+    "lg:px-[180px]",
+    "flex",
+    "justify-center"
+);
+
+const contactContent = /*html*/ `
+    <div class="flex flex-col lg:flex-row items-center md:items-start gap-[20px] md:gap-[40px] lg:gap-[64px] bg-primary py-[40px] md:py-[60px] lg:py-[120px] px-[24px] md:px-[60px] lg:px-[110px] text-grayLight w-full">
+        <h2 class="font-[Montserrat] font-medium text-[28px] md:text-[36px] lg:text-[40px] md:text-start">
+            Zostańmy w kontakcie!
+            <br /> 
+            Znajdziesz nas na 
+            <span class="italic">Instagramie</span>.
+        </h2>
+        <div class="flex flex-col gap-[20px] items-center md:items-start w-[165px]">
+            <p class="text-[16px] md:text-start">Śledź nasze najnowsze realizacje!</p>
+            ${buttonPrimary(
+                "Instagram",
+                "text-primary border-grayLight bg-grayLight hover:text-accent hover:bg-primaryLight hover:border-accent"
+            )}
+        </div>
+    </div>
+`;
+
+contact.innerHTML = contactContent;
+
+export default contact;
--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import header from "./components/header";
 import intro from "./components/intro";
 import offer from "./components/offer";
 import { realization } from "./components/realization";
+import contact from "./components/contact";
 
 const app = document.querySelector("#app");
 
@@ -15,10 +16,13 @@ app.appendChild(intro);
 app.appendChild(offer);
 app.appendChild(aboutFirm);
 app.appendChild(realization);
+app.appendChild(contact);
+
 window.onload = () => {
     const masonry = new Masonry(".masonry-grid ", {
         // options...
         gutter: 30,
+        fitWidth: true,
         originLeft: true,
         horizontalOrder: true,
         itemSelector: ".grid-item",
