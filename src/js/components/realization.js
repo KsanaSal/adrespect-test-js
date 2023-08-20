252ee83f2cee9805a020cfe4997d14dde926898a
KsanaSal <oasalivon@gmail.com>
2023-08-20 16:02:51 +0300

    added images

--- src/js/components/realization.js
+++ src/js/components/realization.js
@@ -1,30 +1,146 @@
 const realization = document.createElement("section");
 realization.classList.add("grid");
+import photo1 from "../../images/imageGallery/photo1.webp";
+import photo2 from "../../images/imageGallery/photo2.webp";
+import photo3 from "../../images/imageGallery/photo3.webp";
+import photo4 from "../../images/imageGallery/photo4.webp";
+import photo5 from "../../images/imageGallery/photo5.webp";
+import photo6 from "../../images/imageGallery/photo6.webp";
+import photo7 from "../../images/imageGallery/photo7.webp";
+import photo8 from "../../images/imageGallery/photo8.webp";
+import photo9 from "../../images/imageGallery/photo9.webp";
+import photo10 from "../../images/imageGallery/photo10.webp";
+import photo11 from "../../images/imageGallery/photo11.webp";
+import photo12 from "../../images/imageGallery/photo12.webp";
+import photo13 from "../../images/imageGallery/photo13.webp";
+import photo14 from "../../images/imageGallery/photo14.webp";
+import photo15 from "../../images/imageGallery/photo15.webp";
+import photo16 from "../../images/imageGallery/photo16.webp";
+import photo17 from "../../images/imageGallery/photo17.webp";
+import photo18 from "../../images/imageGallery/photo18.webp";
+import photo19 from "../../images/imageGallery/photo19.webp";
+import photo20 from "../../images/imageGallery/photo20.webp";
+import photo21 from "../../images/imageGallery/photo21.webp";
+import photo22 from "../../images/imageGallery/photo22.webp";
+import photo23 from "../../images/imageGallery/photo23.webp";
+import photo24 from "../../images/imageGallery/photo24.webp";
+import photo25 from "../../images/imageGallery/photo25.webp";
+import photo26 from "../../images/imageGallery/photo26.webp";
+import photo27 from "../../images/imageGallery/photo27.webp";
+import photo28 from "../../images/imageGallery/photo28.webp";
 
 const imageList = [
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img22.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo1,
+        alt: "Ogród dwupoziomowy",
     },
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img5.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo2,
+        alt: "Altana z basenem",
     },
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img3.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo3,
+        alt: "Rosarium",
     },
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img2.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo4,
+        alt: "Altana z ogrodem",
     },
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img1.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo5,
+        alt: "Jezioro",
     },
     {
-        src: "https://cdn2.hubspot.net/hubfs/4795157/Idxcorporation_December2018/Images/architecturalmillwork/img7.jpg",
-        alt: "Architectural Mill Work 22",
+        src: photo6,
+        alt: "Ścieżka w ogrodzie",
+    },
+    {
+        src: photo7,
+        alt: "Ogród w budynku",
+    },
+    {
+        src: photo8,
+        alt: "Patio",
+    },
+    {
+        src: photo9,
+        alt: "Taras z kwiatami",
+    },
+    {
+        src: photo10,
+        alt: "Alpejski ogród",
+    },
+    {
+        src: photo11,
+        alt: "Altana z ogrodem",
+    },
+    {
+        src: photo12,
+        alt: "Obszar rekreacyjny",
+    },
+    {
+        src: photo13,
+        alt: "Trochę odpoczynku",
+    },
+    {
+        src: photo14,
+        alt: "Ogród na schodach",
+    },
+    {
+        src: photo15,
+        alt: "Alpejskie jezioro",
+    },
+    {
+        src: photo16,
+        alt: "Fontanna z jeziorem",
+    },
+    {
+        src: photo17,
+        alt: "Ścieżka w ogrodzie",
+    },
+    {
+        src: photo18,
+        alt: "Mini altana",
+    },
+    {
+        src: photo19,
+        alt: "Loft na patio",
+    },
+    {
+        src: photo20,
+        alt: "Mini patio i ogród",
+    },
+    {
+        src: photo21,
+        alt: "Ścieżka do Krainy Oz",
+    },
+    {
+        src: photo22,
+        alt: "Letni prysznic",
+    },
+    {
+        src: photo23,
+        alt: "Ścieżka plażowa",
+    },
+    {
+        src: photo24,
+        alt: "Małe patio na poddaszu",
+    },
+    {
+        src: photo25,
+        alt: "Obszar rekreacyjny",
+    },
+    {
+        src: photo26,
+        alt: "Nowoczesny styl",
+    },
+    {
+        src: photo27,
+        alt: "Fontanna",
+    },
+    {
+        src: photo28,
+        alt: "Minimalizm",
     },
 ];
 
